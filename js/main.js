59d583ad4d538fdfa0839a7c283d7ddbf5e29b7a
Shyam Mohan <shyam.mohan@techtaliya.com>
2026-03-27 12:20:05 +0530

    carosausel modification

--- js/main.js
+++ js/main.js
@@ -92,6 +92,7 @@
 	    animateOut: 'fadeOut',
 	    animateIn: 'fadeIn',
 	    nav:false,
+	    dots: false,
 	    autoplayHoverPause: false,
 	    items: 1,
 	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
